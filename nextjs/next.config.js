98796b2722c1e8391cd9ce34d5d50a33e30719bd
Franklin Javier <franklinjalves@gmail.com>
2021-06-07 12:32:50 -0300

    initial

---
+++ nextjs/next.config.js
@@ -0,0 +1,9 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withPlugins([
+  [withBundleAnalyzer]
+  // your other plugins here
+]);
